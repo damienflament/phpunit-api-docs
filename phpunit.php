2433554791c6e3a6bd0c96cf22d885a1f7d42cb8
Damien Flament <damien.flament@gmx.com>
2017-02-01 22:14:30 +0100

    Write main index file to build directory.

--- phpunit.php
+++ phpunit.php
@@ -3,14 +3,15 @@
 use Sami\Sami;
 use Sami\RemoteRepository\GitHubRemoteRepository;
 use Sami\Version\GitVersionCollection;
+use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Finder\Finder;
 
-const REPO         = 'sebastianbergmann/phpunit';
-const REPO_DIR     = __DIR__ . '/phpunit';
-const SRC_DIR      = REPO_DIR . '/src';
-const CACHE_DIR    = __DIR__ . '/cache/%version%';
-const BUILD_DIR    = __DIR__ . '/gh-pages/%version%';
-const TEMPLATE_DIR = __DIR__ . '/templates';
+const REPO           = 'sebastianbergmann/phpunit';
+const REPO_DIR       = __DIR__ . '/phpunit';
+const SRC_DIR        = REPO_DIR . '/src';
+const CACHE_DIR      = __DIR__ . '/cache';
+const BUILD_DIR      = __DIR__ . '/gh-pages';
+const TEMPLATE_DIR   = __DIR__ . '/templates';
 
 
 // Documentation is generated for PHPUnit source files.
@@ -30,7 +31,13 @@ $versions = GitVersionCollection::create(REPO_DIR)
 $loader = new Twig_Loader_Filesystem(TEMPLATE_DIR);
 $twig = new Twig_Environment($loader);
 
-$template = $twig->load('index.html.twig');
+$renderedHtml = $twig->render('index.html.twig');
+
+
+// Write it to build directory.
+$filesystem = new FileSystem();
+
+$filesystem->dumpFile(BUILD_DIR . '/index.html', $renderedHtml);
 
 
 // Return Sami configuration
@@ -38,6 +45,6 @@ return new Sami($iterator, array(
     'title'               => 'PHPUnit API - The PHP Unit Testing framework.',
     'remote_repository'   => new GitHubRemoteRepository(REPO, REPO_DIR),
     'versions'            => $versions,
-    'cache_dir'           => CACHE_DIR,
-    'build_dir'           => BUILD_DIR
+    'cache_dir'           => CACHE_DIR . '/%version%',
+    'build_dir'           => BUILD_DIR . '/%version%'
 ));
