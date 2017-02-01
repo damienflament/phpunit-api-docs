32f7aa43f30cbd85eb99fefa22b3bf7c34614f52
Damien Flament <damien.flament@gmx.com>
2017-02-01 19:26:39 +0100

    Reformat Sami config file.

--- phpunit.php
+++ phpunit.php
@@ -5,9 +5,9 @@ use Sami\RemoteRepository\GitHubRemoteRepository;
 use Sami\Version\GitVersionCollection;
 use Symfony\Component\Finder\Finder;
 
-const REPO = 'sebastianbergmann/phpunit';
-const REPO_DIR = __DIR__ . '/phpunit';
-const SRC_DIR = REPO_DIR . '/src';
+const REPO      = 'sebastianbergmann/phpunit';
+const REPO_DIR  = __DIR__ . '/phpunit';
+const SRC_DIR   = REPO_DIR . '/src';
 const CACHE_DIR = __DIR__ . '/cache/%version%';
 const BUILD_DIR = __DIR__ . '/gh-pages/%version%';
 
@@ -21,9 +21,9 @@ $versions = GitVersionCollection::create(REPO_DIR)
     ->add('5.7', '5.7 (current stable)');
 
 return new Sami($iterator, array(
-    'title'             => 'PHPUnit API - The PHP Unit Testing framework.',
-    'remote_repository' => new GitHubRemoteRepository(REPO, REPO_DIR),
-    'versions'          => $versions,
-    'cache_dir'         => CACHE_DIR,
-    'build_dir'         => BUILD_DIR
+    'title'               => 'PHPUnit API - The PHP Unit Testing framework.',
+    'remote_repository'   => new GitHubRemoteRepository(REPO, REPO_DIR),
+    'versions'            => $versions,
+    'cache_dir'           => CACHE_DIR,
+    'build_dir'           => BUILD_DIR
 ));
