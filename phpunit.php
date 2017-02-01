1b94c2a11c84ba12e36c9e83caa3987131174cd3
Damien Flament <damien.flament@gmx.com>
2017-02-01 06:41:51 +0100

    Add initial Sami configuration.

---
+++ phpunit.php
@@ -0,0 +1,21 @@
+<?php
+
+use Sami\Sami;
+use Sami\RemoteRepository\GitHubRemoteRepository;
+use Symfony\Component\Finder\Finder;
+
+const REPO = 'phpunit/phpunit';
+const REPO_DIR = __DIR__ . '/phpunit';
+const SRC_DIR = REPO_DIR . '/src';
+const CACHE_DIR = __DIR__ . '/cache';
+
+$iterator = Finder::create()
+    ->files()
+    ->name('*.php')
+    ->in(SRC_DIR);
+
+return new Sami($iterator, array(
+    'title'             => 'PHPUnit API',
+    'remote_repository' => new GitHubRemoteRepository(REPO, REPO_DIR),
+    'cache_dir'            => CACHE_DIR
+));
